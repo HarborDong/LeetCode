b0375a4aadbc237584cd45729fea530ed3027780
HarborDong <HarborDong@gmail.com>
2017-12-09 22:23:54 +0800

    -Backtracking -Medium

---
+++ C++/46.Permutations.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void permuteHelper(vector<int>& nums, vector<int>::iterator startIndex, vector<vector<int>>& res) {
+        vector<int>::iterator index =startIndex, i;
+        int temp = 0;
+        if(startIndex == nums.end() - 1) {
+            res.push_back(nums);
+        }
+        
+        for(index = startIndex; index < nums.end(); index++) {
+            //if(index != startIndex && *index == temp) continue;
+            temp= *index;
+            for(i = index; i > startIndex; i--) {
+                *i = *(i-1);
+            }
+            *startIndex = temp;
+            permuteHelper(nums, startIndex + 1, res);
+            temp= *startIndex;
+            for(i = startIndex; i < index; i++) {
+                *i = *(i+1);
+            }
+            *index = temp;
+        }
+        return;
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        permuteHelper(nums, nums.begin(), res);
+        return res;
+    }
+};
